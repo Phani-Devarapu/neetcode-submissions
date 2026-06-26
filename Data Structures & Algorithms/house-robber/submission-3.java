3e1dfacf668f754937e39e298beef52cfadbd781
Phani-Devarapu <70983537+Phani-Devarapu@users.noreply.github.com>
2026-06-25 23:25:22 -0400

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] memo = new int[nums.length];
+        Arrays.fill(memo, -1);
+     return   dp(nums,0,memo);
+    }
+
+    public int dp(int[] nums, int i, int[] memo){
+        if(i >= nums.length){
+            return 0;
+        }
+
+        if(memo[i] == -1){
+            int rob = nums[i] + dp(nums, i+2,memo);
+            int skip = dp(nums,i+1, memo);
+            memo[i] = Math.max(rob,skip);
+        }
+
+        return memo[i];
+    }
+}
